62a14df7162aa251f1668f9ae3e74483a103920d
MOHAMED SHEHAS V <mohamedshehas.v2@mindtree.com>
2025-04-06 11:54:19 +0530

    socket basic connection setup added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { resolve } = require('path');
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3001;
 
 app.use(express.static('static'));
 const cors = require('cors');
@@ -15,11 +15,59 @@ const socketIO = require('socket.io')(http, {
   },
 });
 
+let roomsArr = []
+
 socketIO.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
   socket.on('disconnect', () => {
-    console.log('🔥: A user disconnected');
+    roomsArr = roomsArr.filter((room) => {
+      return room.userId != socket.id
+    })
+    console.log('🔥: user disconnected');
   });
+  socket.on('createRoom',(data) => {
+    socket.join(data.roomName)
+    roomsArr.push({
+      roomName: data.roomName,
+      solution: data.solution,
+      userId: socket.id,
+      members: [socket.id]
+    })
+    console.log(`${data.name} Created the room: ${data.roomName}`);
+  })
+
+  socket.on('joinRoom',(data) => {
+    const roomIndex = roomsArr.findIndex((room) => room.roomName == data.roomName)
+    if(roomIndex > -1){
+      if(roomsArr[roomIndex].members?.length >= 2){
+        socket.emit('roomFull',{
+          message: 'Room is full'
+        })
+      }else{
+        socket.join(data.roomName)
+        roomsArr[roomIndex].members.push(socket.id)
+        console.log(`${data.name} Joined the room: ${data.roomName}`);
+        const solution = roomsArr[roomsArr.findIndex((room) => room.roomName == data.roomName)]?.solution
+        console.log(roomsArr[roomIndex].members)
+        socket.to(data.roomName).emit('validRoom',{
+          message: 'Room is valid',
+          roomName: data.roomName,
+          solution: solution
+        })
+        socket.emit('validRoom',{
+          message: 'Room is valid',
+          roomName: data.roomName,
+          solution: solution
+        })
+        
+      }
+    }else{
+      socket.emit('invalidRoom',{
+        message: 'Room is invalid'
+      })
+    }
+  })
+
 });
 
 app.get('/', (req, res) => {
@@ -28,6 +76,4 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+http.listen(port, () => console.log(`Listening on port ${port}`));
