65cbd1159ed8a407bf764fe54b6b4b16d73d212a
MOHAMED SHEHAS <EYMVIUTHULLIYIL+etihad@etihad.ae>
2026-04-30 00:37:11 +0530

    feat: rejoin after connection issue

--- index.js
+++ index.js
@@ -16,16 +16,25 @@ const socketIO = require('socket.io')(http, {
 });
 
 let roomsArr = []
+const disconnectTimeouts = new Map();
 
 socketIO.on('connection', (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
   socket.on('disconnect', () => {
     roomsArr.forEach((room) => {
-      if (room.members.some(member => member.id === socket.id)) {
-        socket.to(room.roomName).emit('playerDisconnected', { message: 'Opponent disconnected' });
+      const memberIndex = room.members.findIndex(member => member.id === socket.id);
+      if (memberIndex > -1) {
+        socket.to(room.roomName).emit('playerDisconnectedTemporarily', { message: 'Opponent disconnected, waiting for reconnection...' });
+        
+        const timeoutId = setTimeout(() => {
+          socket.to(room.roomName).emit('playerDisconnected', { message: 'Opponent disconnected permanently' });
+          roomsArr = roomsArr.filter((r) => r.roomName !== room.roomName);
+          disconnectTimeouts.delete(room.roomName + '_' + socket.id);
+        }, 15000); // 15-second grace period
+
+        disconnectTimeouts.set(room.roomName + '_' + socket.id, timeoutId);
       }
     });
-    roomsArr = roomsArr.filter((room) => !room.members.some(member => member.id === socket.id));
     console.log('🔥: user disconnected');
   });
   socket.on('createRoom',(data) => {
@@ -73,6 +82,28 @@ socketIO.on('connection', (socket) => {
     }
   })
 
+  socket.on('rejoinRoom', (data) => {
+    const room = roomsArr.find((room) => room.roomName === data.roomName);
+    if (room) {
+      const memberIndex = room.members.findIndex(member => member.name === data.name);
+      if (memberIndex > -1) {
+        const oldSocketId = room.members[memberIndex].id;
+        const timeoutId = disconnectTimeouts.get(room.roomName + '_' + oldSocketId);
+        
+        if (timeoutId) {
+          clearTimeout(timeoutId);
+          disconnectTimeouts.delete(room.roomName + '_' + oldSocketId);
+        }
+        
+        // Update to new socket ID and rejoin the socket.io room channel
+        room.members[memberIndex].id = socket.id;
+        socket.join(data.roomName);
+        
+        socket.to(data.roomName).emit('playerReconnected', { message: 'Opponent reconnected' });
+      }
+    }
+  });
+
   socket.on('playerWon', (data) => {
     socket.to(data.roomName).emit('opponentWon', {
       message: 'Opponent has won the game'
@@ -91,6 +122,36 @@ socketIO.on('connection', (socket) => {
         socket.emit('matchTied');
       }
     }
+  });
+
+  socket.on('requestPlayAgain', (data) => {
+    const roomIndex = roomsArr.findIndex((room) => room.roomName == data.roomName);
+    if(roomIndex > -1){
+      roomsArr[roomIndex].nextSolution = data.newSolution;
+      socket.to(data.roomName).emit('playAgainRequested', {
+        message: 'Opponent wants to play again.'
+      });
+    }
+  });
+
+  socket.on('acceptPlayAgain', (data) => {
+    const roomIndex = roomsArr.findIndex((room) => room.roomName == data.roomName);
+    if(roomIndex > -1){
+      roomsArr[roomIndex].solution = roomsArr[roomIndex].nextSolution;
+      roomsArr[roomIndex].failedCount = 0;
+      socket.to(data.roomName).emit('playAgainAccepted', {
+        newSolution: roomsArr[roomIndex].solution
+      });
+      socket.emit('playAgainAccepted', {
+        newSolution: roomsArr[roomIndex].solution
+      });
+    }
+  });
+
+  socket.on('declinePlayAgain', (data) => {
+    socket.to(data.roomName).emit('playAgainDeclined', {
+      message: 'Opponent declined the rematch.'
+    });
   });
 
 });
